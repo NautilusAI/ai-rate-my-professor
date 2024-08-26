bbe45632973d1cffe4272cc564c437a32c855f0e
Ray Zana <rayzana280@gmail.com>
2024-08-26 02:13:58 -0400

    adding ui and fixing minor error

--- app/page.js
+++ app/page.js
@@ -11,13 +11,13 @@ export default function Home() {
      }
   ])
 
-  const [messages, setMessages] = useState('')
+  const [message, setMessage] = useState('')
   
   const sendMessage = async () => {
     setMessages((messages) => [
       ...messages,
-      {role: "user", content: message}
-      {role: "assistant", content: ' '}
+      {role: "user", content: message},
+      {role: "assistant", content: ' '},
     ])
 
     setMessages('' )
@@ -34,17 +34,17 @@ export default function Home() {
 
       let results =  ''
 
-      return reader.read().then(funciton processText({done, value}) {
+      return reader.read().then(function processText({done, value}) {
         if (done) {
           return results
         }
-        const text = decoder.decode(value || new Unit8Array, {stream: true })
-        setMessages((message) => {
+        const text = decoder.decode(value || new Unit8Array(), {stream: true })
+        setMessages((messages) => {
           let lastMessage = messages[messages.length - 1]
           let otherMessages = messages.slice(0, messages.length - 1) 
           return [
             ...otherMessages,
-            {...lastMessage, lastMessage.content + text},
+            {...lastMessage, content: lastMessage.content + text},
           ]  
         })
         return reader.read().then(processText)
@@ -54,5 +54,54 @@ export default function Home() {
   }
   
 
-  return <></>
+  return(
+    <Box 
+    width="100vw" 
+    height="100vh" 
+    display="flex" 
+    flexDirection="column" 
+    justifyContent="center" 
+    alignItems="center">
+      <Stack 
+      direction="column" 
+      width="500px" 
+      height="700px"
+      border="1px solid black"
+      p={2}
+      spacing={3}
+      >
+        <Stack 
+        direction="column" 
+        spacing={2} 
+        flexgrow={1} 
+        overflow="auto" 
+        maxheight="100%">
+        {message.map((message,index)=>(
+          <Box key={index} display="flex" justifyContent={message.role === "assistant" ? 'flex-start' : 'flex-end'}>
+            <Box 
+            bgcolor={message.role === "assistant" ? 'primary.main' : 'secondary.main'}
+            color="white"
+            borderRadius={16}
+            p={3}
+            >{message.content}</Box>
+          </Box>
+        ))}
+        </Stack>
+        <Stack direction="row" spacing={2}>
+          <TextField 
+          label="Message"
+          fullWidth
+          value={message}
+          onChange={(e)=>{
+            setMessage(e.target.value)
+          }}
+          />
+          <Button variant="contained" onClick={sendMessage}>
+            SEND
+          </Button>
+        </Stack>
+      </Stack>
+    </Box>
+  )
+
 }
